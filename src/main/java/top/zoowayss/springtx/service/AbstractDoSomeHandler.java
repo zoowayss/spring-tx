d4cd9a77c8bb6dac1943faee9e9f344f22479ab5
zoowayss <zoowayss@coldlake.cn>
2024-05-16 16:39:38 +0800

    feat: test autowired subClass

--- src/main/java/top/zoowayss/springtx/service/AbstractDoSomeHandler.java
+++ src/main/java/top/zoowayss/springtx/service/AbstractDoSomeHandler.java
@@ -2,18 +2,22 @@ package top.zoowayss.springtx.service;
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
 public abstract class AbstractDoSomeHandler implements DoSomeThingHandler, InitializingBean {
 
 
-    @Autowired
+    /**
+     * 不能注入自己的子类
+     */
+//    @Autowired
     private List<XXService> xxServices;
 
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        System.out.println(xxServices.size());
+        System.out.println(CollectionUtils.isEmpty(xxServices)?0:xxServices.size());
     }
 }
